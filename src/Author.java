a4e2a0f1e51f946e7bac22e1f949fde719cada22
Abdallah Jaber <abdallahj708@gmail.com>
2022-11-26 18:43:52 -0600

    Updated Biography Practice Repo

--- src/Author.java
+++ src/Author.java
@@ -7,19 +7,39 @@ public class Author {
      */
 
     //Create 6 args custom constructor here
-    //YOUR CODE HERE
-
+    public Author(String firstName, String lastName, String country, String isAlive, int age, Book listOfBooks) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.isAlive = isAlive;
+        this.age = age;
+        this.listOfBooks = listOfBooks;
+    }
 
     /*
         Define instance variables here
         NOTE: Books must be defined as a List of Objects of Book class not String
     */
-    //YOUR CODE HERE
+    String firstName;
+    String lastName;
+    String country;
+    String isAlive;
+    int age;
+    Book listOfBooks;
 
 
     /*
     Override toString() method here that returns Author object information
      */
-    //YOUR CODE HERE
-
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                ", isAlive='" + isAlive + '\'' +
+                ", age=" + age +
+                ", listOfBooks=" + listOfBooks +
+                '}';
+    }
 }
